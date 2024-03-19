ffa5e00e1ebf4eca8ce3d33f2644475ea023f27f
TheBestPetr <ipetrazhitski@gmail.com>
2024-03-19 18:15:58 +0300

    V1 posts & blogs

--- src/posts/postsRouter.ts
+++ src/posts/postsRouter.ts
@@ -3,7 +3,7 @@ import {GetPostsController} from "./getPostsController";
 import {CreatePostController} from "./createPostController";
 import {FindPostController} from "./findPostController";
 import {UpdatePostController} from "./updatePostController";
-import {DeleteBlogController} from "../blogs/deleteBlogController";
+import {DeletePostController} from "./deletePostController";
 
 export const postsRouter = Router()
 
@@ -11,4 +11,4 @@ postsRouter.get('/', GetPostsController)
 postsRouter.post('/', CreatePostController)
 postsRouter.get('/:id', FindPostController)
 postsRouter.put('/:id', UpdatePostController)
-postsRouter.delete('/:id', DeleteBlogController)
+postsRouter.delete('/:id', DeletePostController)
