69053aa85d174a7dfdc73e2c4fa0a802ce32507b
TheBestPetr <ipetrazhitski@gmail.com>
2024-03-22 20:41:43 +0300

    fix status 400

--- src/middlewares/blogsValidationMiddleware.ts
+++ src/middlewares/blogsValidationMiddleware.ts
@@ -1,5 +1,16 @@
 import {body} from "express-validator"
 
-    export const blogNameValidator = body('name').isString().isLength({min: 1, max: 15})
-    export const blogDescriptionValidator = body('description').isString().isLength({min: 1, max: 500})
-    export const blogWebsiteUrlValidator = body('websiteUrl').isString().isLength({min: 10, max: 100}).matches('https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')
+    export const blogNameValidator = body('name')
+        .isString()
+        .isLength({min: 1, max: 15})
+        .withMessage({errorsMessages: [{message: 'Incorrect name', field: 'name'}]})
+    export const blogDescriptionValidator = body('description')
+        .isString()
+        .isLength({min: 1, max: 500})
+        .withMessage({errorsMessages: [{message: 'Incorrect description', field: 'description'}]})
+    export const blogWebsiteUrlValidator = body('websiteUrl')
+        .isString()
+        .isLength({min: 10, max: 100})
+        .matches('https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')
+        .withMessage({errorsMessages: [{message: 'Incorrect websiteUrl', field: 'websiteUrl'}]})
+
--- src/middlewares/postsValidationMiddleware.ts
+++ src/middlewares/postsValidationMiddleware.ts
@@ -1,7 +1,16 @@
 import {body} from "express-validator";
 //import {db} from "../db/db";
 
-export const postTitleValidator = body('title').isString().isLength({min: 1, max: 30})
-export const postShortDescriptionValidator = body('shortDescription').isString().isLength({min: 1, max: 100})
-export const postContentValidator = body('content').isString().isLength({min: 1, max: 1000})
+export const postTitleValidator = body('title')
+    .isString()
+    .isLength({min: 1, max: 30})
+    .withMessage({errorsMessages: [{message: 'Incorrect title', field: 'title'}]})
+export const postShortDescriptionValidator = body('shortDescription')
+    .isString()
+    .isLength({min: 1, max: 100})
+    .withMessage({errorsMessages: [{message: 'Incorrect shortDescription', field: 'shortDescription'}]})
+export const postContentValidator = body('content')
+    .isString()
+    .isLength({min: 1, max: 1000})
+    .withMessage({errorsMessages: [{message: 'Incorrect content', field: 'content'}]})
 //export const postBlogIdValidator = body('blogId').isString().equals(db.blogs.findIndex(b => b.id === 'blogId').toString())
