125a9b8202aa81f8f522ddb7e2a2a0884a1334b4
TheBestPetr <ipetrazhitski@gmail.com>
2024-04-25 21:58:25 +0300

    commit

--- src/middlewares/blogsValidationMiddleware.ts
+++ src/middlewares/blogsValidationMiddleware.ts
@@ -2,9 +2,25 @@ import {body} from "express-validator"
 
 export const blogNameValidator = body('name')
     .isString()
+    .notEmpty()
+    .custom(value => {
+            if (value.includes('   ')) {
+                throw new Error()
+            }
+            return true
+        }
+    )
     .isLength({min: 1, max: 15})
 export const blogDescriptionValidator = body('description')
     .isString()
+    .notEmpty()
+    .custom(value => {
+            if (value.includes('   ')) {
+                throw new Error()
+            }
+            return true
+        }
+    )
     .isLength({min: 1, max: 500})
 export const blogWebsiteUrlValidator = body('websiteUrl')
     .isString()
--- src/middlewares/postsValidationMiddleware.ts
+++ src/middlewares/postsValidationMiddleware.ts
@@ -2,12 +2,36 @@ import {body} from "express-validator";
 
 export const postTitleValidator = body('title')
     .isString()
+    .notEmpty()
+    .custom(value => {
+            if (value.includes('   ')) {
+                throw new Error()
+            }
+            return true
+        }
+    )
     .isLength({min: 1, max: 30})
 export const postShortDescriptionValidator = body('shortDescription')
     .isString()
+    .notEmpty()
+    .custom(value => {
+            if (value.includes('   ')) {
+                throw new Error()
+            }
+            return true
+        }
+    )
     .isLength({min: 1, max: 100})
 export const postContentValidator = body('content')
     .isString()
+    .notEmpty()
+    .custom(value => {
+            if (value.includes('   ')) {
+                throw new Error()
+            }
+            return true
+        }
+    )
     .isLength({min: 1, max: 1000})
 export const postBlogIdValidator = body('blogId')
     .isString()
