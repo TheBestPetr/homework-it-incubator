41a26cc7462dddca1e7d811b43284c66b97d6dd0
TheBestPetr <ipetrazhitski@gmail.com>
2024-05-12 18:31:34 +0300

    hw 6.8

--- src/middlewares/authBearerMiddleware.ts
+++ src/middlewares/authBearerMiddleware.ts
@@ -17,7 +17,7 @@ export const authBearerMiddleware = (req: Request,
     header('authorization')
         .custom(value => {
             const token = value.split(' ')[1]
-            if (token.not().isJWT()) {
+            if (!token.contains('.')) {
                 return 401
             }
             return true
