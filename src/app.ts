efb441b753ddc9baebaad8d567a82a2350cfbf95
TheBestPetr <ipetrazhitski@gmail.com>
2024-05-18 18:16:03 +0300

    hw 7.5

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express"
+import cookieParser from "cookie-parser";
 
 export const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.status(200).json('Hello from IT-INCUBATOR')
